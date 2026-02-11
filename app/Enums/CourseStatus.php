a11fda4f04e3c780ca48f645914540be2f9f0a94
BARHAMA <suleimannmukhtar@gmail.com>
2026-02-11 08:09:03 +0100

    progress

---
+++ app/Enums/CourseStatus.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum CourseStatus: string {
+    case PUBLISHED ='published';
+    case DRAFTED = 'drafted';
+}
--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services
 
+use App\Enums\CourseStatus;
 use App\Models\Course;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,12 +14,7 @@ class CourseService {
             'title' => $data['title'],
             'description' => $data ['description'],
             'teacher_id' => Auth::id(),
-            'is_published' => $data  {enum  {
-                case ;
-            } {
-                case ;
-            }
-
+            'is_published' => $data ['status'] === CourseStatus::PUBLISHED -> value,
         ]);
     }
 
