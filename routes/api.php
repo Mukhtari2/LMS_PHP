01cb1491ec9847f23ba8ebc26f4383365f04d7c6
BARHAMA <suleimannmukhtar@gmail.com>
2026-02-17 17:43:24 +0100

    progress

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,17 @@ Route::post('/login', [AuthApiController::class, 'login']);
 // Protected Endpoints (Requires Bearer Token)
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/admin/create-user', [AuthApiController::class, 'createUser']);
-    Route::post('teacher/course', [CourseApiController::class, 'store']);
-    Route::get('student/courses/{course}', [CourseApiController::class, 'show']);
-    Route::put('teacher/courses/{course}', [CourseApiController::class, 'update']);
-    Route::delete('teacher/courses/{course}', [CourseApiController::class, 'destroy']);
-    Route::post('student/enrollCourse', [EnrollmentApiController::class, 'store']);
-    Route::post('teacher/lesson', [LessonApiController::class, 'store']);
-    Route::put('teacher/lessons/{lesson}', [LessonApiController::class, 'update']);
-    Route::post('student/submission', [SubmissionApiController::class, 'store']);
-    Route::put('teacher/submissions/{submission}/grade', [SubmissionApiController::class, 'grade']);
-    Route::post('teacher/assignments', [AssignmentApiController::class, 'store']);
-    Route::get('student/assignments/{id}', [AssignmentApiController::class, 'show']);
-    Route::put('student/assignments/{assignment}', [AssignmentApiController::class, 'update']);
+    Route::post('/course', [CourseApiController::class, 'store']);
+    Route::get('/courses/{course}', [CourseApiController::class, 'show']);
+    Route::put('/courses/{course}', [CourseApiController::class, 'update']);
+    Route::delete('/courses/{course}', [CourseApiController::class, 'destroy']);
+    Route::post('/enrollCourse', [EnrollmentApiController::class, 'store']);
+    Route::post('/lesson', [LessonApiController::class, 'store']);
+    Route::put('/lessons/{lesson}', [LessonApiController::class, 'update']);
+    Route::post('/submission', [SubmissionApiController::class, 'store']);
+    Route::put('/submissions/{submission}/grade', [SubmissionApiController::class, 'grade']);
+    Route::post('/assignments', [AssignmentApiController::class, 'store']);
+    Route::get('/assignments/{id}', [AssignmentApiController::class, 'show']);
+    Route::put('/assignments/{assignment}', [AssignmentApiController::class, 'update']);
 });
 
