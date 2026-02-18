0c0e51385ea1c2f2d238b8cbd2c2e913cae29093
BARHAMA <suleimannmukhtar@gmail.com>
2026-02-18 10:31:51 +0100

    progress

--- routes/api.php
+++ routes/api.php
@@ -8,22 +8,33 @@ use App\Http\Controllers\Api\SubmissionApiController;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpKernel\HttpCache\Store;
 
+// Public
 Route::post('/login', [AuthApiController::class, 'login']);
 
-// Protected Endpoints (Requires Bearer Token)
+// Protected (All logged in users)
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/admin/create-user', [AuthApiController::class, 'createUser']);
-    Route::post('/course', [CourseApiController::class, 'store']);
-    Route::get('/courses/{course}', [CourseApiController::class, 'show']);
-    Route::put('/courses/{course}', [CourseApiController::class, 'update']);
-    Route::delete('/courses/{course}', [CourseApiController::class, 'destroy']);
-    Route::post('/enrollCourse', [EnrollmentApiController::class, 'store']);
-    Route::post('/lesson', [LessonApiController::class, 'store']);
-    Route::put('/lessons/{lesson}', [LessonApiController::class, 'update']);
-    Route::post('/submission', [SubmissionApiController::class, 'store']);
-    Route::put('/submissions/{submission}/grade', [SubmissionApiController::class, 'grade']);
-    Route::post('/assignments', [AssignmentApiController::class, 'store']);
-    Route::get('/assignments/{id}', [AssignmentApiController::class, 'show']);
-    Route::put('/assignments/{assignment}', [AssignmentApiController::class, 'update']);
-});
 
+    // ADMIN ONLY
+    Route::middleware('role:admin')->group(function () {
+        Route::post('/admin/create-user', [AuthApiController::class, 'createUser']);
+    });
+
+    // TEACHER ONLY
+    Route::middleware('role:teacher')->group(function () {
+        Route::post('courses', [CourseApiController::class, 'store']);
+        Route::put('courses/{course}', [CourseApiController::class, 'update']);
+        Route::delete('courses/{course}', [CourseApiController::class, 'destroy']);
+        Route::post('lessons', [LessonApiController::class, 'store']);
+        Route::put('lessons/{lesson}', [LessonApiController::class, 'update']);
+        Route::post('assignments', [AssignmentApiController::class, 'store']);
+        Route::put('submissions/{submission}/grade', [SubmissionApiController::class, 'grade']);
+    });
+
+    // STUDENT ONLY
+    Route::middleware('role:student')->group(function () {
+        Route::get('courses/{course}', [CourseApiController::class, 'show']);
+        Route::post('enroll', [EnrollmentApiController::class, 'store']);
+        Route::post('submissions', [SubmissionApiController::class, 'store']);
+        Route::get('assignments/{id}', [AssignmentApiController::class, 'show']);
+    });
+});
