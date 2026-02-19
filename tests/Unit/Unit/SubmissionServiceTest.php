e94e2da7866f375ffca0ae7283c0c1b2498ea6f2
BARHAMA <suleimannmukhtar@gmail.com>
2026-02-19 13:10:23 +0100

    progress

--- tests/Unit/Unit/SubmissionServiceTest.php
+++ tests/Unit/Unit/SubmissionServiceTest.php
@@ -1,16 +1,54 @@
 <?php
 
-namespace Tests\Unit\Unit;
+namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Services\SubmissionService;
+use App\Models\User;
+use App\Models\Assignment;
+use App\Models\Submission;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
 
 class SubmissionServiceTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    protected SubmissionService $submissionService;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->submissionService = new SubmissionService;
+    }
+
+    #[Test]
+    public function test_to_create_submission(): void
+    {
+
+        $student = User::factory()->create(['role' => 'student']);
+        $assignment = Assignment::factory()->create();
+
+        Storage::fake('public');
+        $file = UploadedFile::fake()->create('assignment_solution.pdf', 100);
+
+        $this->actingAs($student);
+
+           $data = [
+            'assignment_id' => $assignment->id,
+            'student_id'    => $student->id,
+            'grade'         => null,
+            'teacher_feedback' => null,
+        ];
+
+        $submission = $this->submissionService->createSubmission($data, $file);
+
+        $this->assertInstanceOf(Submission::class, $submission);
+        $this->assertDatabaseHas('submissions', [
+            'assignment_id' => $assignment->id,
+            'user_id'       => $student->id,
+        ]);
     }
 }
