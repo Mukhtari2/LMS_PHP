65990adbc7d97018cae9fbc64b784bc491192105
BARHAMA <suleimannmukhtar@gmail.com>
2026-02-23 15:57:47 +0100

    progress

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -4,16 +4,21 @@ namespace App\Services;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 
 
 class AuthService {
     public function registerUser(array $data): User {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => $data['password'],
+            'password' => Hash::make($data['password']),
             'role' => $data['role'],
         ]);
+
+        event(new Registered($user));
+
+        return $user;
     }
 
     public function login(array $loginDetails){
